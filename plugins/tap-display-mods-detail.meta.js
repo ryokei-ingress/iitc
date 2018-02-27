c035ec0ce2bc711d3b624434f4a4c529bb27cd6b
ryokei-ingress <ryokei.ingress@gmail.com>
2018-02-27 18:33:55 +0900

    adding uploadURL and downloadURL

---
+++ plugins/tap-display-mods-detail.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @id             iitc-plugin-tap-display-mods-detail@ryokei
+// @name           IITC plugin: tap dispaly mods detail
+// @category       Info
+// @version        0.0.2.20180227.1829
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://github.com/ryokei-ingress/iitc/plugins/tap-dispaly-mods-detail.meta.js
+// @downloadURL    https://github.com/ryokei-ingress/iitc/plugins/tap-dispaly-mods-detail.user.js
+// @description    Enables tap to display the detail of mods installed on portals
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
+
